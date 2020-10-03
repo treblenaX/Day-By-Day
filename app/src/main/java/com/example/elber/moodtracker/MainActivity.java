c9f5ecb2c1bbefb3badabf017ecc90fa28cb7962
Alan Wen <alanjameswen@outlook.com>
2020-10-03 14:54:38 -0700

    Main and Calendar Activities (#6)
    * Updated visibility on date object
    
    * PatternEngine and Tests
    
    Base PatternEngine class with corresponding Tests. Actual code to diagnose illnesses not yet written.
    
    * PatternEngine Illness Diagnosis
    
    Added diagnosis methods to Illnesses. Engine can now warn of illnesses.
    
    * MainActivity Layout
    
    * Main and Calendar Activities

--- app/src/main/java/com/example/elber/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/elber/moodtracker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.elber.moodtracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // on Confirm Button click
+        final Button button = findViewById(R.id.confirmButton);
+        final Context c = this;
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // todo: save data set on sliders
+
+                // todo: see if follow up questions should be asked
+
+                // open Calendar activity
+                Intent i = new Intent(c, Calendar.class);
+                startActivity(i);
+            }
+        });
     }
 }
